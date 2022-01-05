9b5b0758ca1ce1b0d90bb30d372a3bd169535030
Monika <monikaangel.ma15@gmail.com>
2022-01-05 01:54:20 -0800

    finished task 3

--- src/test/java/com/h2/SavingsCalculator.java
+++ src/test/java/com/h2/SavingsCalculator.java
@@ -47,7 +47,7 @@ public class SavingsCalculator {
 
 
         int totalDaysInMonth = yearMonth.lengthOfMonth();
-        int remainingDays = date.getDayOfMonth() - totalDaysInMonth;
+        int remainingDays = totalDaysInMonth - date.getDayOfMonth();
 
         return remainingDays;
     }
