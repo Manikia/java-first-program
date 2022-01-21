193e55f481d393e477f3e30e83bc5908f7acb1be
Monika <monikaangel.ma15@gmail.com>
2022-01-20 21:21:13 -0800

    checking if new class works

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -8,12 +8,40 @@ public class Utilities {
         try
         {
             out = Long.parseLong(in);
-            return out;
         }
         catch (NumberFormatException e)
         {
             throw new IllegalArgumentException(in + " cannot be converted into a 'long' value. Exiting program.");
         }
+        return out;
+    }
+    public static int getIntValue(String in)
+    {
+        int out = Integer.MIN_VALUE;
+
+        try
+        {
+            out = Integer.parseInt(in);
+        }
+        catch(NumberFormatException e)
+        {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'int' value. Exiting program.");
+        }
+        return out;
+    }
+    public static float getFloatValue(String in)
+    {
+        float out = Float.MIN_VALUE;
+
+        try
+        {
+            out = Float.parseFloat(in);
+        }
+        catch(NumberFormatException e)
+        {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'float' value. Exiting program.");
+        }
+        return out;
     }
 
 }
